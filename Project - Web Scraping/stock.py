741c7e016c86d868d8dbe5cad2f0849a1ffee8a0
Emile Drolet <emilio12321@hotmail.com>
2024-05-23 20:04:51 -0400

    Add Web Scraping Project

---
+++ Project - Web Scraping/stock.py
@@ -0,0 +1,157 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+
+# TODO Have one excel only, or can be an option
+
+# TODO Add the corresponding method for each parameter
+
+# TODO Problem with consent(), put exception somehting like that. There is 2 cases.
+
+class Stock:
+
+    def __init__(self, ticker: str, income_statement: bool, balance_sheet: bool, cash_flow: bool,
+                 expand_all: bool, summary: bool):
+        # Summary
+        self.ticker = ticker
+
+        # Pages chose
+        self.isIncomeStatement = income_statement
+        self.isBalanceSheet = balance_sheet
+        self.isCashFlow = cash_flow
+        self.isSummary = summary
+
+        # Options
+        self.isExpandAll = expand_all
+
+        # Others
+        self.driver = None
+        self.oneTime = 0
+
+    def web_scraping(self):
+        self.getBrowser()
+        self.openNewWindow()
+        # self.consent()
+
+        # TODO Can do un for loop maybe. Need a list of each option or something like that
+        if self.isIncomeStatement:
+            self.getFinancialStatement("financials")
+            self.openNewWindow()
+        if self.isBalanceSheet:
+            self.getFinancialStatement("balance-sheet")
+            self.openNewWindow()
+        if self.isCashFlow:
+            self.getFinancialStatement("cash-flow")
+            self.openNewWindow()
+        if self.isSummary:
+            self.getSummary()
+            self.openNewWindow()
+
+        self.driver.quit()
+
+    def getSummary(self):
+        self.driver.get(f"https://finance.yahoo.com/quote/{self.ticker}?p={self.ticker}")
+
+        if self.oneTime == 0:
+            self.consent()
+            self.oneTime += 1
+
+        summary_titles = self.driver.find_elements(By.CSS_SELECTOR, value='td[class="C($primaryColor) W(51%)"] span ')
+        summary_data = self.driver.find_elements(By.CSS_SELECTOR, value='td[class="Ta(end) Fw(600) Lh(14px)"]')
+
+        summary_title_list = [title.text for title in summary_titles]
+        summary_data_list = [x.text for x in summary_data]
+        name_columns = ["Title", "Value"]
+
+        tableau = pd.DataFrame()
+
+        tableau[name_columns[0]] = summary_title_list
+        tableau[name_columns[1]] = summary_data_list
+
+        tableau.to_excel(f'output_{self.ticker}_summary.xlsx', index=False)
+
+    def getBrowser(self):
+        # Keep Chrome browser open after program finishes
+        chrome_option = webdriver.ChromeOptions()
+        chrome_option.add_experimental_option("detach", True)
+
+        # Which browser to choose. This is a class.
+        driver = webdriver.Chrome(options=chrome_option)
+
+        self.driver = driver
+
+    def openNewWindow(self):
+        # Open a new window (javascript)
+        self.driver.execute_script("window.open('', '_blank');")
+
+        # Retrieve all the window
+        window_handles = self.driver.window_handles
+
+        # Switch to the last window
+        self.driver.switch_to.window(window_handles[-1])
+
+    def consent(self):
+        # url_consent = 'https://consent.yahoo.com/v2/collectConsent?sessionId=1_cc-session_d6058863-b799-4e26-a4fd-7cc201d915d7'
+        # self.driver.get(url_consent)
+        consent = self.driver.find_element(By.XPATH,
+                                           value='//*[@id="consent-page"]/div/div/div/form/div[2]/div[2]/button')
+        consent.click()
+
+    def getFinancialStatement(self, financial_statement):
+
+        self.driver.get(f"https://finance.yahoo.com/quote/{self.ticker}/{financial_statement}")
+
+        if self.oneTime == 0:
+            self.consent()
+            self.oneTime += 1
+
+        # Click on Expand all
+        if self.isExpandAll:
+            expand = self.driver.find_element(By.XPATH,
+                                              value='//*[@id="nimbus-app"]/section/section/section/article/article/div/div[2]/button')
+            expand.click()
+
+        # Récuperer les data
+        financial_data = self.driver.find_elements(By.CSS_SELECTOR,
+                                                   value='div[class="column svelte-1xjz32c"], div[class="column svelte-1xjz32c alt"]')
+
+        names_column = self.driver.find_elements(By.CSS_SELECTOR, value='div[class="sticky column svelte-1ezv2n5"], div[class="column svelte-1ezv2n5"], div[class="column svelte-1ezv2n5 alt"]')
+        names_row = self.driver.find_elements(By.CSS_SELECTOR,
+                                              value='div[class="rowTitle svelte-1xjz32c"]')
+
+
+        # Mettre les data dans des listes
+        financial_data_list = [data.text for data in financial_data]
+        names_column_list = [data.text for data in names_column]
+        names_row_list = [data.text for data in names_row]
+
+        NUMBER_OF_COLUMNS = len(names_column_list)
+
+        print(financial_data_list)
+        print(names_column_list)
+        print(names_row_list)
+        print(NUMBER_OF_COLUMNS)
+
+        print(len(financial_data_list))
+        print(len(names_column_list))
+        print(len(names_row_list))
+
+
+
+        # Initialisation du tableau
+        tableau = pd.DataFrame()
+
+        # Append the tableau
+        for i in range(0, NUMBER_OF_COLUMNS):
+            if i == 0:
+                # Premiere colonne
+                tableau[names_column_list[i]] = names_row_list
+            else:
+                # Autre colonne
+                tableau[names_column_list[i]] = financial_data_list[i - 1::NUMBER_OF_COLUMNS-1]
+
+
+        tableau.to_excel(f'output_{self.ticker}_{financial_statement}.xlsx', index=False)
+
+        print(tableau)
---
+++ Project - Web Scraping/web_scraping.py
@@ -0,0 +1,119 @@
+from tkinter import *
+from stock import Stock
+
+
+# TODO Add widget for each option
+def process(stock):
+    ticker = Stock(stock, income_statement=feature_income, balance_sheet=feature_balance, cash_flow=feature_cash,
+                   expand_all=feature_expand_all, summary=feature_summary)
+    ticker.web_scraping()
+
+    window_enter = Toplevel(window)
+    center_window(window_enter)
+    window_enter.minsize(width=300, height=200)
+    window_enter.title("Confirmation")
+    confirmation_label = Label(window_enter, text="Le telechargement est pret")
+    confirmation_label.grid()
+def center_window(window):
+    window.update_idletasks()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    window.geometry(f"{width}x{height}+{x}+{y}")
+def button_clicked():
+    stock = stock_input.get()
+    process(stock)
+def income_clicked():
+    global feature_income
+    if isIncomeStatement.get() == 1:
+        feature_income = True
+def balance_clicked():
+    global feature_balance
+    if isBalanceSheet.get() == 1:
+        feature_balance = True
+def cash_clicked():
+    global feature_cash
+    if isCashFlow.get() == 1:
+        feature_cash = True
+
+def summary_clicked():
+    global feature_summary
+    if isSummary.get() == 1:
+        feature_summary = True
+
+
+
+#TODO Humm should not use Global variable,as little as possible
+
+# Initialisiton Global Variable
+feature_income = False
+feature_balance = False
+feature_cash = False
+feature_expand_all = True
+feature_summary = False
+
+
+window = Tk()
+window.title("Web Scraping Financial Data")
+window.eval('tk::PlaceWindow . center')
+window.minsize(width=500, height=300)
+center_window(window)
+
+# Label Logo
+my_label_logo = Label(text="Logo")
+my_label_logo.grid(column=0, row=0)
+
+# Label Stock
+my_label_stock = Label(text="stock")
+my_label_stock.grid(column=0, row=1)
+
+# Button
+button_enter = Button(text="Enter", command=button_clicked)
+button_enter.grid(column=0, row=3)
+
+# Entry for stock
+stock_input = Entry(width=10)
+stock_input.grid(column=1, row=1)
+
+# Check Box - Income Statement
+isIncomeStatement = IntVar()
+checkIncomeStatement = Checkbutton(variable=isIncomeStatement, onvalue=1, offvalue=0, command=income_clicked)
+checkIncomeStatement.grid(column=0, row=4)
+
+# Label Income Statement
+my_label_income= Label(text="income statement")
+my_label_income.grid(column=1, row=4)
+
+# Check Box - Balance Sheet
+isBalanceSheet = IntVar()
+checkBalanceSheet = Checkbutton(variable=isBalanceSheet, onvalue=1, offvalue=0, command=balance_clicked)
+checkBalanceSheet.grid(column=0, row=5)
+
+# Label Balance Sheet
+my_label_balance= Label(text="balance sheet")
+my_label_balance.grid(column=1, row=5)
+
+# Check Box - Cash Flow
+isCashFlow = IntVar()
+checkCashFlow = Checkbutton(variable=isCashFlow, onvalue=1, offvalue=0, command=cash_clicked)
+checkCashFlow.grid(column=0, row=6)
+
+# Label Cash Flow
+my_label_cash= Label(text="cash flow")
+my_label_cash.grid(column=1, row=6)
+
+# Check Box - Summary
+isSummary = IntVar()
+checkSummary = Checkbutton(variable=isSummary, onvalue=1, offvalue=0, command=summary_clicked)
+checkSummary.grid(column=0, row=7)
+
+# Label Summary
+my_label_summary = Label(text="summary")
+my_label_summary.grid(column=1, row=7)
+
+
+
+window.mainloop()
