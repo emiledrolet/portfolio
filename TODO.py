3d633125408520d682e474eac975858639729787
Emile Drolet <emilio12321@hotmail.com>
2024-05-23 22:32:15 -0400

    Add External Css file

--- TODO.py
+++ TODO.py
@@ -1,10 +1,13 @@
 # COURT TERME
 
+
 # TODO Commencer projet Suivi performance d'un portefeuille
 # TODO Ensuite faire ML project
 
 
 # LONG TERME
 
+# TODO create de base html
+# TODO ajouter style avec JavaScript
 # TODO Create good visual with bootstrap
 # TODO Ameliorer son code avec apprentissage (data algorithm)
