a57888402bdfc583b4847d9cac3d90338f2656aa
Emile Drolet <emilio12321@hotmail.com>
2024-05-23 22:11:06 -0400

    Apparition dossier

--- TODO.py
+++ TODO.py
@@ -1,2 +1,10 @@
+# COURT TERME
+
+# TODO Commencer projet Suivi performance d'un portefeuille
+# TODO Ensuite faire ML project
+
+
+# LONG TERME
+
 # TODO Create good visual with bootstrap
 # TODO Ameliorer son code avec apprentissage (data algorithm)
