898bd370c7c3422f95ad66303583637bc06e7b55
Emile Drolet <emilio12321@hotmail.com>
2024-07-23 23:01:15 -0400

    retrieve data from yahoo

--- Project - Suivi Performance PF/main.py
+++ Project - Suivi Performance PF/main.py
@@ -2,10 +2,26 @@ import pandas as pd
 import numpy as np
 import pyfolio as pf
 import matplotlib.pyplot as plt
-from pypfopt.efficient_frontier import EfficientFrontier
-from pypfopt import risk_models, expected_returns
 import seaborn as sns
-from pandas_datareader.data import DataReader
+import yfinance as yf
 from datetime import date
-import statsmodels.api as sm
 
+
+# import statsmodels.api as sm
+# from pypfopt.efficient_frontier import EfficientFrontier
+# from pypfopt import risk_models, expected_returns
+
+start = date(2015, 1, 1)
+end = date(2016, 1, 1)
+
+ticker = "GOOG"
+
+# Convert dates to string format for yfinance
+start_str = start.strftime('%Y-%m-%d')
+end_str = end.strftime('%Y-%m-%d')
+
+# Fetch data using yfinance
+stock_data = yf.download(ticker, start=start_str, end=end_str)
+
+stock_data_close = stock_data["Close"]
+print(stock_data_close.pct_change())
